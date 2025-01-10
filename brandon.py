a4fa1c83d479a12ab4060a0f5be5fe637fc7ef62
Monko2k <kevinhuang2k@gmail.com>
2025-01-09 22:49:02 -0500

    temporarily disable purge

--- brandon.py
+++ brandon.py
@@ -114,6 +114,7 @@ async def on_member_join(member):
 
 @tasks.loop(hours=config["purgeIntervalHours"])
 async def purge():
+    return
     cutoff = datetime.datetime.now() - datetime.timedelta(hours=config["inactivityLimitHours"])
     cursor = db.find({})
     members = {}
